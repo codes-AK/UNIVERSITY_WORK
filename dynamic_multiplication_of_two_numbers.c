67600943947e622bbbee074ece093e17a3f7293a
ak <aksharasajeevanm@gmail.com>
2026-07-03 21:37:34 +0800

    Add dynamic multiplication of two numbers program

---
+++ dynamic_multiplication_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a ;
+    int b ;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the valus of b:");
+    scanf("%d",&b);
+    int c = a*b;
+    printf("the product is:  %d",c);
+}
